cdcf03759ad7db7b6082eb98b5e554b4b146b5cb
曹东 <867137498@qq.com>
2025-04-25 11:08:31 +0800

    feat: add demo

--- AES/main.py
+++ AES/main.py
@@ -5,14 +5,13 @@ key = 'test'
 RoundKeys = aes.round_key_generator(string_to_hex(key))
 
 # 加密
-plaintext = 'plaintext'
-# 0x00112233445566778899aabbccddeeff -> b'\x11"3DUfw\x88\x99\xaa\xbb\xcc\xdd\xee\xff'
+plaintext = 'testtest'
 plaintext = aes.num_2_16bytes(string_to_hex(plaintext))
 ciphertext = aes.aes_encrypt(plaintext, RoundKeys)
 print('ciphertext = ' + hex(aes._16bytes2num(ciphertext)))
 
 # 解密
-ciphertext = 0xe006bc8a15ef40145bc7024e2d2db8e8
+ciphertext = 0x766f8156096dea2340434c3344646819
 ciphertext = aes.num_2_16bytes(ciphertext)
 plaintext = aes.aes_decrypt(ciphertext, RoundKeys)
 print('plaintext = ' + hex_to_string(aes._16bytes2num(plaintext)))
